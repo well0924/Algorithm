dda6c716450b907b3a9ae982cd923ca001fb6e0b
well0924 <well414965@gmail.com>
2023-02-11 21:10:56 +0900

    더블 링크드 리스트 구현실습 및 이진 트리 구현 실습 코드 작성

---
+++ src/Algorithm/DataStructure/chap09/DblLinkedList.java
@@ -0,0 +1,112 @@
+package Algorithm.DataStructure.chap09;
+
+import java.util.Comparator;
+
+public class DblLinkedList<E> {
+	class Node<E>{
+		
+		private E data;
+		
+		private Node<E>prev;
+		
+		private Node<E>next;
+		
+		//생성자
+		Node(){
+			data = null;
+			prev=next=this;
+		}
+		
+		//생성자
+		Node(E obj,Node<E>prev,Node<E>next){
+			data = obj;
+			this.prev = prev;
+			this.next = next;
+		}
+	}
+	
+	private Node<E>head;//머리노드
+	
+	private Node<E>crnt;//선택노드
+	
+	public DblLinkedList(){
+		head = crnt = new Node<E>();
+	}
+	
+	public boolean isEmpty() {
+		return head.next == head;
+	}
+	
+	public E search(E obj,Comparator<? super E>c) {
+		Node<E>ptr = head.next;
+		
+		while(ptr != head) {
+			if(c.compare(obj, ptr.data)==0) {
+				crnt = ptr;
+				return ptr.data;
+			}
+			ptr = ptr.next;
+		}
+		return null;
+	}
+	
+	public void printCurrentNode() {
+		if(isEmpty()) {
+			System.out.println("선택 노드가 없습니다.");
+		}else{
+			System.out.println(crnt.data);
+		}
+	}
+	
+	public void dump() {
+		Node<E>ptr = head.next;
+		
+		while(ptr != head) {
+			System.out.println(ptr.data);
+			ptr = ptr.next;
+		}
+	}
+	
+	public void dumpReverse() {
+		Node<E>ptr = head.prev;
+		
+		while(ptr!= head) {
+			System.out.println(ptr.data);
+			ptr = ptr.prev;
+		}
+	}
+	
+	public boolean next() {
+		if(isEmpty()||crnt.next==head) {
+			return false;
+		}
+		crnt = crnt.next;
+		return true;
+	}
+	
+	public boolean prev() {
+		if(isEmpty()||crnt.prev == head) {
+			return false;
+		}
+		crnt = crnt.next;
+		return true;
+	}
+	
+	public void add(E obj) {
+		Node<E>node = new Node<E>(obj,crnt,crnt.next);
+		crnt.next = crnt.next.prev = node;
+		crnt = node;
+	}
+	
+	public void addFirst(E obj) {
+		crnt = head;
+		add(obj);
+	}
+	
+	public void addLast(E obj) {
+		crnt = head.prev;
+		add(obj);
+	}
+	
+	
+}
--- src/Algorithm/DataStructure/chap10/BinTree.java
+++ src/Algorithm/DataStructure/chap10/BinTree.java
@@ -3,12 +3,15 @@ package Algorithm.DataStructure.chap10;
 import java.util.Comparator;
 
 public class BinTree<K,V> {
+	
 	static class Node<K,V>{
+		
 		private K key;
 		private V data;
 		private Node<K,V>left;
 		private Node<K,V>right;
 		
+		//생성자
 		Node(K key,V data,Node<K,V>left,Node<K,V>right){
 			this.key = key;
 			this.data = data;
@@ -27,9 +30,11 @@ public class BinTree<K,V> {
 		void print() {
 			System.out.println(data);
 		}
+		
 	}
 	
 	private Node<K,V>root;//루트
+	
 	private Comparator<?super K>comparator = null;
 	
 	private int comp(K key1,K key2) {
@@ -37,9 +42,11 @@ public class BinTree<K,V> {
 	}
 	
 	public V search(K key) {
+		
 		Node<K,V>p =root;
 		
 		while(true) {
+			
 			if(p == null) {
 				return null;
 			}
@@ -57,16 +64,20 @@ public class BinTree<K,V> {
 	}
 	
 	private void addNode(Node<K,V>node,K key,V data) {
+	
 		int cond = comp(key, node.getKey());
+		
 		if(cond ==0) {
 			return;
 		}else if(cond < 0) {
+			
 			if(node.left==null) {
 				node.left =new Node<K,V>(key,data,null,null);
 			}else {
 				addNode(node.left,key,data);
 			}
 		}else {
+			
 			if(node.right == null) {
 				node.right = new Node<K,V>(key,data,null,null);
 			}else {
@@ -76,6 +87,7 @@ public class BinTree<K,V> {
 	}
 	
 	public void add(K key,V data) {
+		
 		if(root == null) {
 			root = new Node<K,V>(key,data,null,null);
 		}else {
@@ -86,22 +98,31 @@ public class BinTree<K,V> {
 	public boolean remove(K key) {
 		Node<K,V>p = root;
 		Node<K,V>parent = null;
+		
 		boolean isLeftChild = true;
 		
 		while(true) {
+		
 			if(p == null) {
 				return false;
 			}
+			
 			int cond = comp(key,p.getKey());
+			
 			if(cond == 0) {
 				break;
 			}else {
 				parent = p;
+			
 				if(cond <0) {
+	
 					isLeftChild = true;
+	
 					p = p.left;
 				}else {
+	
 					isLeftChild = false;
+	
 					p = p.right;
 				}
 			}
@@ -116,6 +137,7 @@ public class BinTree<K,V> {
 				parent.right = p.right;
 			}
 		}else if(p.right == null) {
+		
 			if(p == root) {
 				root = p.left;
 			}else if(isLeftChild) {
@@ -125,12 +147,17 @@ public class BinTree<K,V> {
 			}
 		}else {
 			parent = p;
+			
 			Node<K,V>left = p.left;
+			
 			isLeftChild = true;
 			
 			while(left.right != null) {
+			
 				parent = left;
+			
 				left = left.right;
+			
 				isLeftChild = false;
 			}
 			
@@ -149,7 +176,9 @@ public class BinTree<K,V> {
 	private void printSubTree(Node node) {
 		if(node != null) {
 			printSubTree(node.left);
+	
 			System.out.println(node.key+""+node.data);
+			
 			printSubTree(node.right);
 		}
 	}
